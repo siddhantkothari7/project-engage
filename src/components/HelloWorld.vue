72a16d5348140820e864f974d7776d4d86898444
Krish Sapru <ksapru@bu.edu>
2021-03-29 17:27:50 -0400

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,91 +1,75 @@
 <template>
+<center>
+  <v-container 
+    class="ma-14"
+     >
+    <v-row 
+    no-gutters
+    style="height: 300px;">
+      <v-col order="last">  
+        <v-card
+          class="pa-14"
+          outlined
+          tile
+          no-borders          
+        >
 
-    <section class="container-fluid">
-        <div class="row justify-content-center">
-            <div class="col-lg-4">
-                <div class="login-panel bg-white text-left">
-                    <div class="alert alert-primary" role="alert" :style="{opacity: isAlertShow ? 1 : 0}">
-                        Login successfully. <small>waiting for redirect.</small>
-                        <loader-component width="30"></loader-component>
-                    </div>
-                     
-
-    
- 
-                     
-                     <v-img :src="require('../assets/backgroundd.png')" > 
-                     <h2><v-img :src="require('../assets/sparky.png')" height="300px" width="300px" border-radius="100px"></v-img> <h1>Welcome to SPARK!</h1></h2>
-                    
-
-                    
-                     <form action="">
-
-                       
-                        <test>
-
-                   
-                        <div align="right" style="margin-top:-175px; margin-right:230px;" width="200px">
-                            <lable class="input-label"><h3>For Students  </h3></lable>
-                        </div>
+              <center><lable class="input-label"><h3>For Students  </h3></lable></center>
 
                         
 
                        
-                      <div align="right" style="margin-top:0px; margin-right:200px;"><h4>
-              <v-btn class="ma-2" outlined rounded color="black">
-                Kerberos Login
+                     <center><h4>
+              <v-btn class="mx-2" rounded dark color="#23b676">
+               Kerberos Login
               </v-btn></h4>
-            </div>
+            </center>
  
 
             
 
-                        <template>
-            
-                        </template>
-                        <div align="right" style="margin-top:0px; margin-right:230px;">
-                            <lable class="input-label"> <h3>For Admins      </h3> </lable>
-                         </div>
+                         <br/>
+                        <center><lable class="input-label"><h3>For Admins  </h3></lable></center>
 
-                         <div align="right" style="margin-top:0px; margin-right:214px;">
-                        <h4>
-                         <v-btn class="" rounded outlined color="black" > Gmail Login </v-btn> </h4>
-                         </div>
-                         
-                        
- 
-                        <br>
-                        <div class="form-group d-flex justify-content-center">
-                            <button class="btn btn-primary w-25" disabled @click.prevent="login" v-if="isLoggingIn"><loader-component width="30"></loader-component></button>
-                        </div>
-                        </test>
-                    </form>
-                     
-                     
-                     </v-img>
-                    
-                 
-                     
-                    
-                </div>
 
-            </div>
-        </div>
-    </section>
+                               <center><h4>
+              <v-btn class="mx-2" rounded dark color="#23b676">
+               Gmail Login
+              </v-btn></h4>
+            </center>
+                     
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+           
+          <center><v-img
+                :src="require('../assets/sparky.png')"
+                height="200px"
+                width="200px"
+                border-radius="100px"
+              ></v-img></center>
+              <center><h1>Welcome to SPARK!</h1></center>
+        </v-card>
+      </v-col>      
+    </v-row>
+  </v-container> </center>
 </template>
-
-
-
-<script>
+ 
 
  
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+<script>
+  export default {
+    data: () => ({
+      alignments: [
+        'center',
+      ],
+    }),
   }
-}
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -107,8 +91,8 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-a {
-  color: #415a4f;
+v-btn {
+  color: #23b676;
 }
 
 h2 {
