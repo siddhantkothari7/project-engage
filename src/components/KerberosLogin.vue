8693cb73b361db4d75c092b4821ec31523d0050c
Krish Sapru <ksapru@bu.edu>
2021-04-05 15:31:47 -0400

    Create KerberosLogin.vue

---
+++ src/components/KerberosLogin.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>
+        test1
+    </h1>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
