363d1cd89a9529c627811d26fece67d046e48ffa
Krish Sapru <ksapru@bu.edu>
2021-04-05 15:30:47 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
-import router from "./router";
+import VueRouter from "vue-router";
 import store from "./store";
-
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+ 
+ 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
+
+ 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
-}).$mount("#app");
+},
+
+
+
+).$mount("#app");
