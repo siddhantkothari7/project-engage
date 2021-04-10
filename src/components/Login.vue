03f9be52853d2eacf9af638486c7b5f40ca6795d
Krish Sapru <ksapru@bu.edu>
2021-04-10 15:07:29 -0400

    Create Login.vue

---
+++ src/components/Login.vue
@@ -0,0 +1,9 @@
+<template>
+<router-view>
+    <div id="nav">
+        <router-link to="/"></router-link>
+        <router-link to="/KerberosLogin">KerberosLogin</router-link>
+        <router-link to="/GmailLogin">GmailLogin</router-link>
+    </div>
+    </router-view>
+</template>
